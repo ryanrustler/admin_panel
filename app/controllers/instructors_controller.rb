0bd65d7c62f2abc6ff4fbac4d953d9eb7b429c4a
Ryan Robinson <ryanrobinson@Ryans-MacBook-Pro.local>
2019-02-07 09:17:34 -0500

    instructor controllers

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -25,6 +25,19 @@ class InstructorsController < ApplicationController
         @instructor = Instructor.find(params[:id])
     end
 
+    def update
+        @instructor = Instructor.find(params[:id])
+        @instructor.update(instructors_params)
+        redirect_to instructor_path(@instructor)
+    end
+
+    def destroy
+        @instructor = Instructor.find(params[:id])
+        @instructor.destroy
+        redirect_to instructors_path
+    end
+    
+
 
 
     private
